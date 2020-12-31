d1906891ea77ae01f715f5e876113a05653c6568
Francisco Gonzalez <francisco.gonzalez@strong.io>
2020-12-30 21:29:11 -0600

    Update

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 module.exports = {
   //-- SITE SETTINGS -----
   author: "@panch_gonzalez",
-  siteTitle: "Gatsby Starter Portfolio Minimal",
+  siteTitle: "Francisco Gonzalez",
   siteShortTitle: "fg.", // Used as logo text in header, footer, and splash screen
   siteDescription:
-    "A modern one-page portfolio with a clean yet expressive design.",
-  siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+    "Data Science Portfolio.",
+  siteUrl: "https://panch.io",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
-  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  seoTitleSuffix: "Francisco Gonzalez", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
   // -- THEME SETTINGS -----
   colors: {
@@ -91,5 +91,14 @@ module.exports = {
       url: "/#contact",
     },
   },
-  footerLinks: [],
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
 }
